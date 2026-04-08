f8f39e1418de22721fdf4fde85c7c3af83e5eff5
TrumanBenjamin <122401528+TrumanBenjamin@users.noreply.github.com>
2026-04-07 21:18:43 -0600

    updated styling

--- frontend/src/app/part-list/part-list.component.ts
+++ frontend/src/app/part-list/part-list.component.ts
@@ -14,6 +14,7 @@ import { PartEditComponent } from '../part-edit/part-edit.component';
 export class PartListComponent implements OnInit {
   parts: Part[] = [];
   selectedPart: Part | null = null;
+  errorMessage: string = '';
 
   constructor(private partService: PartService) {}
 
